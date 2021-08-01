95cf9556e185552b5ea6e38f75e63730749778eb
Gorakh Raj Joshi <gorakh626@gmail.com>
2021-08-01 20:35:47 +0545

    Multiple states in react

--- src/1-useState/3-useState-Object.js
+++ src/1-useState/3-useState-Object.js
@@ -1,20 +1,25 @@
-import React from 'react'
+import { useState } from 'react'
 
 const UseStateObject = () => {
-  const [person, setPerson] = React.useState({
-    name: 'Ram',
-    age: 32,
-    message: 'Hello, I am Ram!',
-  })
+  //   const [person, setPerson] = React.useState({
+  //     name: 'Ram',
+  //     age: 32,
+  //     message: 'Hello, I am Ram!',
+  //   })
+
+  const [name, setName] = useState('Ram')
+  const [age, setAge] = useState(32)
+  const [message, setMessage] = useState('Hello, I am Ram!')
 
   const changeMessage = () => {
-    setPerson({ ...person, message: 'New Message' })
+    setMessage('New Message')
   }
-
-  console.log(person)
 
   return (
     <>
+      <h3>{name}</h3>
+      <h3>{age}</h3>
+      <h3>{message}</h3>
       <button className='btn' onClick={changeMessage}>
         Change Message
       </button>
