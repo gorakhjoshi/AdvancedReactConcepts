3cd2b37e67751acb2d74fc0fbffcdcb457cf61e8
Gorakh Raj Joshi <gorakh626@gmail.com>
2021-08-05 21:03:32 +0545

    Basic of form and controlled input

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import MultipleReturns from './3-multiple-returns/multiple-returns'
+import FormBasics from './4-react-form/1-form-basics'
 
 function App() {
   return (
     <div className='container'>
-      <MultipleReturns />
+      <FormBasics />
     </div>
   )
 }
