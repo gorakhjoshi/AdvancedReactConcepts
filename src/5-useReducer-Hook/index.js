184f6e0c5a22b8734bf0dd7f010ba1ee7dbfcf6c
Gorakh Raj Joshi <gorakh626@gmail.com>
2021-08-08 22:20:10 +0545

    using useReducer Hook

--- src/5-useReducer-Hook/index.js
+++ src/5-useReducer-Hook/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useReducer } from 'react'
 import Modal from './Modal'
 import { data } from '../data'
 
@@ -7,14 +7,27 @@ const Index = () => {
   const [people, setPeople] = useState(data)
   const [showModal, setShowModal] = useState(false)
 
+  const initialState = {
+      people: data,
+      isModalOpen: false,
+      modalContent: 'Hello'
+  }
+
+  const reducer = (state, action) => {
+      if(action.type === 'TESTING') {
+          return state
+      }
+
+  }
+
+  const [state, dispatch] = useReducer(reducer, initialState)
+
   const handleSubmit = (e) => {
     e.preventDefault()
     if (name) {
-      setShowModal(true)
-      setPeople([...people, { id: new Date().getTime().toString() }])
-      setName('')
+      dispatch({type: 'TESTING' })
     } else {
-      setShowModal(true)
+      
     }
   }
   return (
@@ -34,9 +47,11 @@ const Index = () => {
         <button type='submit'>Add</button>
       </form>
       {people.map((person) => {
-          return <div key = {person.id}>
-              <h4>{person.name}</h4>
+        return (
+          <div key={person.id}>
+            <h4>{person.name}</h4>
           </div>
+        )
       })}
     </>
   )
