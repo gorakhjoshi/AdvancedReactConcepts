d752591c5c6f57707f8ec34753504f3a3eec8a8e
Gorakh Raj Joshi <gorakh626@gmail.com>
2021-08-15 20:19:45 +0545

    prop validation with short circuit

--- src/9-prop-types/Product.js
+++ src/9-prop-types/Product.js
@@ -1,26 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import defaultImage from '../assets/defaultImage.png';
 
 const Product = ({ image, name, price }) => {
+  const url = image && image.url;
   return (
     <article className='product'>
       <h4>{name}</h4>
       <p>{price}</p>
-      <img src={image.url} alt='name' />
+      <img src={url || defaultImage} alt='name' />
     </article>
   );
 };
 
-Product.propTypes = {
-  image: PropTypes.object.isRequired,
-  name: PropTypes.string.isRequired,
-  price: PropTypes.number.isRequired,
-};
+// Product.propTypes = {
+//   image: PropTypes.object.isRequired,
+//   name: PropTypes.string.isRequired,
+//   price: PropTypes.number.isRequired,
+// };
 
-Product.defaultProps = {
-  name: 'my default name',
-  price: 15,
-  image: {},
-};
+// Product.defaultProps = {
+//   name: 'my default name',
+//   price: 15,
+//   image: defaultImage,
+// };
 
 export default Product;
